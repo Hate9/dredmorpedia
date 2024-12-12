0b9b6724748daaaf95dd92e3fc9f8cee9b3ee1a3
Mabel Emillia Schaefer <contact.schaeferwebdev@gmail.com>
2024-12-11 22:18:11 -0500

    added A Debacle of Diggles, Advanced Craft'n'Smelt, ASCII Invasion, Aztechnologist, Cask of Amontillado, Crossbow Overhaul 2, Embreakening, the combined Essential Skills mod, Extra Potions, Hamvil of Krong, Interior Dredmorating, Kingdom of Loathing Classes, King of Birds, Monstrous Megapack, More Daggers, More Fountains, Mo' Steampunk, Polearms 'n' Spears, Swashbucklers, and Tomecraft, and updated FaxPax

--- js/source.js
+++ js/source.js
@@ -63,18 +63,9 @@ Dredmor.Source.List =
 	},
 	{
 		id:     genId(),
-		dir: 	'essential1',
-		tag: 	'E1 Mod',
+		dir: 	'essential',
+		tag: 	'E Mod',
 		title: 	'Essential DoD Mod',
-		href: 	'http://community.gaslampgames.com/threads/essential-dod-skills-i-and-ii-finally-game-ready.1770/',
-		active: false,
-		mod:    true
-	},
-	{
-		id:     genId(),
-		dir: 	'essential2',
-		tag: 	'E2 Mod',
-		title: 	'Essential DoD II Mod',
 		href: 	'http://community.gaslampgames.com/threads/essential-dod-skills-i-and-ii-finally-game-ready.1770/',
 		active: false,
 		mod:    true
@@ -139,6 +130,168 @@ Dredmor.Source.List =
 		tag:	'C Mod',
 		title:  'Chronomancy Mod',
 		href:   'http://community.gaslampgames.com/threads/chronomancy-time-manipulation-skill.2642/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'adebacleofdiggles',
+		tag:	'DD Mod',
+		title:  'Debacle of Diggles Mod',
+		href:   'https://archive-community.dredmor.com/threads/a-debacle-of-diggles.3055/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'advancedcraftnsmelt',
+		tag:	'ACS Mod',
+		title:  'Advanced Craft n Smelt Mod',
+		href:   'https://archive-community.dredmor.com/threads/craft-advanced-craftnsmelt.5082/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'asciiinvasion',
+		tag:	'AI Mod',
+		title:  'ASCII Invasion Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=1980224569',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'aztecnologist',
+		tag:	'AZ Mod',
+		title:  'Aztecnologist Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=3286029943',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'caskofamontillado',
+		tag:	'CA Mod',
+		title:  'Cask of Amontillado Mod',
+		href:   'https://archive-community.dredmor.com/threads/minimod-the-cask-of-amontillado.13826/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'crossbowoverhaul2',
+		tag:	'CO2 Mod',
+		title:  'Crossbow Overhaul 2 Mod',
+		href:   'https://archive-community.dredmor.com/threads/crossbow-overhaul-2-0-90.7291/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'embreakening',
+		tag:	'EB Mod',
+		title:  'Embreakening Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=618960254',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'extrapotions',
+		tag:	'EP Mod',
+		title:  'Extra Potions Mod',
+		href:   'https://archive-community.dredmor.com/threads/extra-potions.3306/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'hamvilofkrong',
+		tag:	'HK Mod',
+		title:  'Hamvil of Krong Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=197949456',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'kingdomofloathingclasses',
+		tag:	'KLC Mod',
+		title:  'Kingdom of Loathing Classes Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=739150441',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'kingofbirds',
+		tag:	'KB Mod',
+		title:  'King of Birds Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=3337462239&searchtext=birds',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'monstrousmegapack',
+		tag:	'MMP Mod',
+		title:  'Monstrous Megapack Mod',
+		href:   'https://archive-community.dredmor.com/threads/monstrous-megapack-monsters-items-rooms-1-2-2-released.5896/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'moredaggers',
+		tag:	'MD Mod',
+		title:  'More Daggers Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=87481373',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'morefountains',
+		tag:	'MF Mod',
+		title:  'More Fountains Mod',
+		href:   'https://archive-community.dredmor.com/threads/more-fountains-mod.7494/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'mosteampunk',
+		tag:	'MS Mod',
+		title:  'Mo Steampunk Mod',
+		href:   'https://archive-community.dredmor.com/threads/mo-steampunk-steampunk-itempack.5006/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'polearmsnspears',
+		tag:	'PS Mod',
+		title:  'Polearms n Spears Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=168054467',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'swashbucklers',
+		tag:	'SB Mod',
+		title:  'Swashbucklers Mod',
+		href:   'https://archive-community.dredmor.com/threads/swashbucklers-mod-more-stuff-for-pirates.3527/',
+		active: false,
+		mod:    true
+	},
+	{
+		id:     genId(),
+		dir:	'tomecraft',
+		tag:	'TC Mod',
+		title:  'Tomecraft Mod',
+		href:   'https://steamcommunity.com/sharedfiles/filedetails/?id=255759636',
 		active: false,
 		mod:    true
 	}
